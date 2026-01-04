837a0d346f48ed8b1197dc221d56c9b86f0d3107
mishobo <husseinabdallah287@gmail.com>
2026-01-04 12:39:08 +0300

    feat: user entity

---
+++ src/main/java/com/husseinmishobo/authorization/entities/User.java
@@ -0,0 +1,39 @@
+package com.husseinmishobo.authorization.entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.util.Date;
+
+@Setter
+@Getter
+@Table(name = "users", schema = "users")
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(nullable = false)
+    private Integer id;
+
+    @Column(nullable = false)
+    private String fullName;
+
+    @Column(unique = true, length = 100, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @CreationTimestamp
+    @Column(updatable = false, name = "created_at")
+    private Date createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private Date updatedAt;
+
+}
